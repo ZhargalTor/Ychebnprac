2d436f459186714945c18bb441cdbeb705cfae33
ZhargalTor <zhargaltor@gmail.com>
2024-10-04 21:29:49 +0800

    Create Main.java

---
+++ Глава 4/Main.java
@@ -0,0 +1,19 @@
+Глава 4 Вариант A задание 10
+Выполнил: Торяшиев Жаргал
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите имя щенка: ");
+        String name = scanner.nextLine();
+        Puppy puppy = new Puppy(name);
+        puppy.makeSound();
+        puppy.jump();
+        puppy.run();
+        puppy.bite();
+        puppy.play();
+        System.out.println("Имя щенка: " + puppy.name);
+    }
+}

d151229e2383619c31bdc68c06d8684a75007c92
ZhargalTor <zhargaltor@gmail.com>
2024-10-04 21:30:42 +0800

    Create Puppy.java

---
+++ Глава 4/Puppy.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Puppy extends Dog {
+    public Puppy(String name) {
+        super(name);
+    }
+
+    public void play() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Щенок играет с кем: ");
+        String withWhom = scanner.nextLine();
+        System.out.println("Щенок играет с " + withWhom);
+    }
+
+    @Override
+    public String toString() {
+        return "Puppy { Имя ='" + name + "' }";
+    }
+}

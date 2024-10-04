16a4b8e4e53663c919d8317962585c6106555417
ZhargalTor <zhargaltor@gmail.com>
2024-10-04 21:30:14 +0800

    Create Animal.java

---
+++ Глава 4/Animal.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Animal {
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void makeSound() {
+        System.out.println("Животное издает звук");
+    }
+
+    public void jump() {
+        System.out.println("Животное прыгает");
+    }
+
+    public void run() {
+        System.out.println("Животное бежит");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(name, animal.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Animal { Имя = '" + name + "' }";
+    }
+}

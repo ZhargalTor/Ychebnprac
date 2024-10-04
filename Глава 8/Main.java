68846b5b0d4830c2264d08509ae3caf96257d496
ZhargalTor <zhargaltor@gmail.com>
2024-10-04 21:32:57 +0800

    Create Main.java

---
+++ Глава 8/Main.java
@@ -0,0 +1,46 @@
+Глава 8 Вариант A задание 10
+Выполнил: Торяшиев Жаргал
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Character, Integer> charCount = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("text.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                for (char c : line.toCharArray()) {
+                    charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка чтения файла: " + e.getMessage());
+        }
+
+        System.out.print("Введите количество символов для вывода: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        System.out.println("Символы, встречающиеся наиболее часто:");
+        int count = 0;
+        while (count < n) {
+            int maxCount = 0;
+            char maxChar = ' ';
+            for (Map.Entry<Character, Integer> entry : charCount.entrySet()) {
+                if (entry.getValue() > maxCount) {
+                    maxCount = entry.getValue();
+                    maxChar = entry.getKey();
+                }
+            }
+
+            if (maxCount == 0) {
+                break; // все символы уже были выведены
+            }
+
+            System.out.println("'" + maxChar + "': " + maxCount);
+            charCount.remove(maxChar);
+            count++;
+        }
+    }
+}
